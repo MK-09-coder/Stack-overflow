aa1c42398cd65d1c172d321b56ffe5d23abefc7f
MK-09-coder <thekumar09mk@gmail.com>
2023-04-08 14:42:13 +0530

    lastly

--- client/src/pages/Questions/QuestionsDetails.jsx
+++ client/src/pages/Questions/QuestionsDetails.jsx
@@ -70,7 +70,7 @@ const QuestionsDetails = () => {
       const dispatch=useDispatch()
       const User=useSelector((state)=>(state.currentUserReducer))
       const location=useLocation()
-      const url='http://localhost:3000'
+      const url='https://stack-overflow-remake.netlify.app'
       const handlePostAns = (e,answerLength) => {
             e.preventDefault()
             if(User===null){
